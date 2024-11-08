2951102f9e764176362b564bf31a70e91f2f43fe
karmen-tran <tran.karmen3@gmail.com>
2024-11-08 10:18:21 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='module 9 homework',
+    url='https://github.com/karmen-tran/m09-demo',
+    author='Karmen Tran',
+    author_email='aqq2ex@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
