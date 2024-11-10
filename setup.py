b3dcb15f17c2dc9a338c0a2f4920db96d25162b0
karmen-tran <tran.karmen3@gmail.com>
2024-11-10 11:33:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='m09-demo',
+    name='booklover',
     version='0.1',
     description='module 9 homework',
     url='https://github.com/karmen-tran/m09-demo',
